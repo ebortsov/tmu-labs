924840470a5971a3bd32f2119189aaabc6265af5
Evgenii Bortsov <ebortsoff@yandex.ru>
2026-06-16 21:40:50 +0300

    eval passes for mandelbrot

--- sheet02/mandelbrot/eval.sh
+++ sheet02/mandelbrot/eval.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 rm -rf build
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER="clang++-18" -DCMAKE_C_COMPILER="clang-18" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_C_COMPILER="clang" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --target tests
 
 FAILED=0
--- sheet02/mandelbrot/mandelbrot.cpp
+++ sheet02/mandelbrot/mandelbrot.cpp
@@ -2,20 +2,20 @@
 #include <cstdio>
 
 namespace {
-    // check if the imaginary number z = real + i*img belongs to mandelbrot set
-    bool belongs_to_mandelbrot(double real, double img) {
-        double z_real = 0, z_img = 0;
-        for (int i = 1; i <= 200; i++) {
-            double new_z_real = z_real * z_real - z_img * z_img + real;
-            double new_z_img = 2 * z_real * z_img + img;
-            z_real = new_z_real;
-            z_img = new_z_img;
-            if (z_real * z_real + z_img * z_img > 4) {
-                return false;
-            }
+// check if the imaginary number z = real + i*img belongs to mandelbrot set
+bool belongs_to_mandelbrot(double real, double img) {
+    double z_real = 0, z_img = 0;
+    for (int i = 1; i <= 200; i++) {
+        double new_z_real = z_real * z_real - z_img * z_img + real;
+        double new_z_img = 2 * z_real * z_img + img;
+        z_real = new_z_real;
+        z_img = new_z_img;
+        if (z_real * z_real + z_img * z_img > 4) {
+            return false;
         }
-        return true;
     }
+    return true;
+}
 } // namespace
 
 namespace mandelbrot {
